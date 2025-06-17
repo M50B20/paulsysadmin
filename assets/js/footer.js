7d5c50891ac58298062840168fd9a693e0ca6e27
M50B20 <124495728+M50B20@users.noreply.github.com>
2025-06-17 13:53:34 +0200

    update

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,6 +1,6 @@
 fetch('../components/footer.html')
   .then(res => res.text())
   .then(html => {
-    document.getElementById('header-container').innerHTML = html;
+    document.getElementById('footer').innerHTML = html;
     initThemeToggle();
   });
--- assets/js/header.js
+++ assets/js/header.js
@@ -1,6 +1,6 @@
 fetch('../components/header.html')
   .then(res => res.text())
   .then(html => {
-    document.getElementById('header-container').innerHTML = html;
+    document.getElementById('header').innerHTML = html;
     initThemeToggle();
   });
