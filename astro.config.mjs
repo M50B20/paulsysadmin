8a48a925548e5afa8a88b6eb8b142b9a85f1750a
M50B20 <Paul28535@gmx.fr>
2025-11-08 12:44:32 +0100

    Restauration de la configuration Astro avec Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  base: '/', 
+  site: 'https://paulsysadmin.fr',
+  base: '/',
+  integrations: [tailwind()],
 });
