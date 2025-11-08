ddbdc113b423f41cf39d0c2127d192710718eff5
M50B20 <Paul28535@gmx.fr>
2025-11-08 19:05:09 +0100

    ajout de sitemap pour l'indexation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://paulsysadmin.fr',
@@ -7,7 +8,8 @@ export default defineConfig({
   integrations: [
     tailwind({
       config: { applyBaseStyles: false }
-    })
+    }),
+    sitemap()
   ],
   build: {
     assets: 'assets'
